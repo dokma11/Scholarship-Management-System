7abe52ecec462f4b77c5702e0e0fc9289ae58a08
dokma11 <vule.dok@gmail.com>
2024-05-21 20:41:17 +0200

    feat: Initialize student endpoint

---
+++ SMS-Service/SMS-Service/Model/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace SMS_Service.Model
+{
+    public class Student
+    {
+        private int StudentID { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Email { get; set; }
+        private string DateOfBirth { get; set; }
+        private Education education { get; set; }
+        private double GPA { get; set; }
+    }
+
+
+    public enum Education
+    {
+        HighSchool,
+        University
+    }
+}
---
+++ SMS-Service/SMS-Service/Repository/Implementation/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Service.Repository.Implementation
+{
+    public class IStudentRepository
+    {
+    }
+}
