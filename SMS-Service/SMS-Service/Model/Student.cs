8458ec7497e193959303bb21fbb699db00d02a62
dokma11 <vule.dok@gmail.com>
2024-05-21 20:58:59 +0200

    feat: Implement serialization

--- SMS-Service/SMS-Service/Model/Student.cs
+++ SMS-Service/SMS-Service/Model/Student.cs
@@ -1,14 +1,45 @@
-﻿namespace SMS_Service.Model
+﻿using SMS_Service.Serialization;
+using System;
+
+namespace SMS_Service.Model
 {
-    public class Student
+    public class Student : ISerializable
     {
-        private int StudentID { get; set; }
+        private int ID { get; set; }
         private string FirstName { get; set; }
         private string LastName { get; set; }
         private string Email { get; set; }
         private string DateOfBirth { get; set; }
         private Education education { get; set; }
         private double GPA { get; set; }
+
+        public Student() { }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                ID.ToString(),
+                FirstName,
+                LastName,
+                Email,
+                DateOfBirth,
+                education.ToString(),
+                GPA.ToString()
+            };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            ID = Convert.ToInt32(values[0]);
+            FirstName = values[1];
+            LastName = values[2];
+            Email = values[3];
+            DateOfBirth = values[4];
+            education = (Education)Enum.Parse(typeof(Education), values[5]);
+            GPA  = Convert.ToDouble(values[6]);
+        }
     }
 
 
@@ -17,4 +48,5 @@
         HighSchool,
         University
     }
+
 }
---
+++ SMS-Service/SMS-Service/Serialization/Serializer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SMS_Service.Serialization
+{
+    class Serializer<T> where T : ISerializable, new()
+    {
+        private const char DELIMITER = '|';
+
+        public void ToCSV(string fileName, List<T> objects)
+        {
+            StringBuilder csv = new StringBuilder();
+
+            foreach (T obj in objects)
+            {
+                string line = string.Join(DELIMITER.ToString(), obj.ToCSV());
+                csv.AppendLine(line);
+            }
+
+            File.WriteAllText(fileName, csv.ToString());
+
+        }
+
+        public List<T> FromCSV(string fileName)
+        {
+            List<T> objects = new List<T>();
+
+            if (!File.Exists(fileName))
+            {
+                FileStream fs = File.Create(fileName);
+                fs.Close();
+            }
+
+            foreach (string line in File.ReadLines(fileName))
+            {
+                string[] csvValues = line.Split(DELIMITER);
+                T obj = new T();
+                obj.FromCSV(csvValues);
+                objects.Add(obj);
+            }
+
+            return objects;
+        }
+    }
+}
