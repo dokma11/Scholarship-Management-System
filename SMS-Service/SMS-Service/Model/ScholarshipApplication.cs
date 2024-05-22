0fcab71fcc9411f3ef837338f9936cd1b8d8b310
dokma11 <vule.dok@gmail.com>
2024-05-22 17:39:46 +0200

    feat: Initialize ScholarshipApplication model

---
+++ SMS-Service/SMS-Service/Model/ScholarshipApplication.cs
@@ -0,0 +1,50 @@
+﻿using SMS_Service.Serialization;
+using System;
+
+namespace SMS_Service.Model
+{
+    public class ScholarshipApplication: ISerializable
+    {
+        public int ID { get; set; }
+        public ScholarshipApplicationStatus Status { get; set; }
+        public DateTime SubmissionDate { get; set; }
+        public DateTime ApprovalDate { get; set; }
+
+        public ScholarshipApplication() { }
+
+        public ScholarshipApplication(int iD, ScholarshipApplicationStatus status, DateTime submissionDate, DateTime approvalDate)
+        {
+            ID = iD;
+            Status = status;
+            SubmissionDate = submissionDate;
+            ApprovalDate = approvalDate;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+                ID.ToString(),
+                Status.ToString(),
+                SubmissionDate.ToString(),
+                ApprovalDate.ToString(),
+            };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            ID = Convert.ToInt32(values[0]);
+            Status = (ScholarshipApplicationStatus)Enum.Parse(typeof(ScholarshipApplicationStatus), values[1]);
+            SubmissionDate = Convert.ToDateTime(values[2]);
+            ApprovalDate = Convert.ToDateTime(values[3]);
+        }
+    }
+
+    public enum ScholarshipApplicationStatus
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+}
