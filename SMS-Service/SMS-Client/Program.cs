33de099467cd911586f3c13012119ba3f396b0ff
dokma11 <vule.dok@gmail.com>
2024-06-09 12:26:19 +0200

    feat: Implement client scholarship operations

--- SMS-Service/SMS-Client/Program.cs
+++ SMS-Service/SMS-Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using StudentContractService;
+using ScholarshipContractService;
 
 namespace SMS_Client
 {
@@ -30,7 +31,7 @@ namespace SMS_Client
                     case 4:
                         Console.WriteLine("Client is closing...");
                         running = false;
-                        return;
+                        break;
                     default:
                         Console.WriteLine("Invalid option. Please try again.");
                         break;
@@ -113,10 +114,17 @@ namespace SMS_Client
                         break;
                     case 4:
                         var students = client.GetAllAsync();
-                        foreach (var student in students.Result)
+                        if(students.Result.Length == 0)
                         {
-                            Console.WriteLine($"ID: {student.ID}, Name: {student.FirstName}, Surname: {student.LastName}, GPA: {student.GPA}, " +
-                                $"Email: {student.Email}, Date of birth: {student.DateOfBirth}");
+                            Console.WriteLine("There are no students");
+                        }
+                        else
+                        {
+                            foreach (var student in students.Result)
+                            {
+                                Console.WriteLine($"ID: {student.ID}, Name: {student.FirstName}, Surname: {student.LastName}, GPA: {student.GPA}, " +
+                                    $"Email: {student.Email}, Date of birth: {student.DateOfBirth}");
+                            }
                         }
                         break;
                     case 5:
@@ -138,7 +146,121 @@ namespace SMS_Client
 
         private static void HandleScholarshipOperations()
         {
-            // Similar structure to HandleStudentOperations
+            Console.WriteLine("\nYou have successfully chosen scholarship operations!\n");
+            Console.WriteLine("Choose one of the scholarship operations: ");
+            Console.WriteLine("1. Create");
+            Console.WriteLine("2. Update");
+            Console.WriteLine("3. Delete");
+            Console.WriteLine("4. Get all");
+            Console.WriteLine("5. Get by ID");
+            Console.WriteLine("6. Exit");
+
+            int scholarshipNumber = int.Parse(Console.ReadLine());
+            using (var client = new ScholarshipContractClient())
+            {
+                switch (scholarshipNumber)
+                {
+                    case 1:
+                        var newScholarship = new Scholarship();
+                        Console.WriteLine("Enter name: ");
+                        newScholarship.Name = Console.ReadLine();
+                        Console.WriteLine("Enter description: ");
+                        newScholarship.Description = Console.ReadLine();
+                        Console.WriteLine("Enter amount: ");
+                        newScholarship.Amount = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter deadline: ");
+                        newScholarship.Deadline = DateTime.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter GPA requirement: ");
+                        newScholarship.GPARequirement = double.Parse(Console.ReadLine());                        
+                        Console.WriteLine("Enter minimum age: ");
+                        newScholarship.MinAge = int.Parse(Console.ReadLine());                        
+                        Console.WriteLine("Enter maximum age: ");
+                        newScholarship.MaxAge = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter education: 1. High school 2. University");
+                        if (Console.ReadLine() == "1")
+                        {
+                            newScholarship.Education = ScholarshipContractService.StudentEducation.HighSchool;
+                        }
+                        else if (Console.ReadLine() == "2")
+                        {
+                            newScholarship.Education = ScholarshipContractService.StudentEducation.University;
+                        }
+                        Console.WriteLine("Enter student limit: ");
+                        newScholarship.StudentLimit = int.Parse(Console.ReadLine());
+                        newScholarship.IsDeleted = false;
+                        client.CreateAsync(newScholarship).Wait();
+                        Console.WriteLine("Scholarship created successfully.");
+                        break;
+                    case 2:
+                        var updateScholarship = new Scholarship();
+                        Console.WriteLine("Enter id of scholarship that you want to update");
+                        updateScholarship.ID = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter name: ");
+                        updateScholarship.Name = Console.ReadLine();
+                        Console.WriteLine("Enter description: ");
+                        updateScholarship.Description = Console.ReadLine();
+                        Console.WriteLine("Enter amount: ");
+                        updateScholarship.Amount = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter deadline: ");
+                        updateScholarship.Deadline = DateTime.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter GPA requirement: ");
+                        updateScholarship.GPARequirement = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter minimum age: ");
+                        updateScholarship.MinAge = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter maximum age: ");
+                        updateScholarship.MaxAge = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter education: 1. High school 2. University");
+                        if (Console.ReadLine() == "1")
+                        {
+                            updateScholarship.Education = ScholarshipContractService.StudentEducation.HighSchool;
+                        }
+                        else if (Console.ReadLine() == "2")
+                        {
+                            updateScholarship.Education = ScholarshipContractService.StudentEducation.University;
+                        }
+                        Console.WriteLine("Enter student limit: ");
+                        updateScholarship.StudentLimit = int.Parse(Console.ReadLine());
+                        client.UpdateAsync(updateScholarship).Wait();
+                        Console.WriteLine("Scholarship updated successfully.");
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter scholarship ID to delete:");
+                        int idToDelete = int.Parse(Console.ReadLine());
+                        client.DeleteAsync(idToDelete).Wait();
+                        Console.WriteLine("Scholarship deleted successfully.");
+                        break;
+                    case 4:
+                        var scholarships = client.GetAllAsync();
+                        if(scholarships.Result.Length == 0)
+                        {
+                            Console.WriteLine("There are no scholarships");
+                        }
+                        else
+                        {
+                            foreach (var scholarship in scholarships.Result)
+                            {
+                                Console.WriteLine($"ID: {scholarship.ID}, Name: {scholarship.Name}, Description: {scholarship.Description}, Amount: {scholarship.Amount}, " +
+                                    $"Deadline: {scholarship.Deadline}, GPA requirement: {scholarship.GPARequirement}, Minimum age: {scholarship.MinAge}, Maximum age: {scholarship.MaxAge}, " +
+                                    $"Student limit: {scholarship.StudentLimit}, Education: {scholarship.Education}");
+                            }
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter scholarship ID:");
+                        int idToGet = int.Parse(Console.ReadLine());
+                        var scholarshipById = client.GetByIdAsync(idToGet);
+                        Console.WriteLine($"ID: {scholarshipById.Result.ID}, Name: {scholarshipById.Result.Name}, Description: {scholarshipById.Result.Description}, Amount: {scholarshipById.Result.Amount}, " +
+                                                        $"Deadline: {scholarshipById.Result.Deadline}, GPA requirement: {scholarshipById.Result.GPARequirement}, Minimum age: {scholarshipById.Result.MinAge}, Maximum age: {scholarshipById.Result.MaxAge}, " +
+                                                        $"Student limit: {scholarshipById.Result.StudentLimit}, Education: {scholarshipById.Result.Education}"); 
+                        break;
+                    case 6:
+                        Console.WriteLine("Client is closing...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
         }
 
         private static void HandleApplicationManagement()
