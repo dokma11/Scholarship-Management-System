140f0602ed92bf1b513340a54a0beac3fa5efc8b
dokma11 <vule.dok@gmail.com>
2024-06-09 12:26:47 +0200

    feat: Add logs

--- SMS-Service/SMS-Service/Contract/Implementation/ScholarshipContract.svc.cs
+++ SMS-Service/SMS-Service/Contract/Implementation/ScholarshipContract.svc.cs
@@ -16,26 +16,32 @@ namespace SMS_Service.Contract
 
         public void Create(Scholarship scholarship)
         {
+            Console.WriteLine("Scholarship contract create");
+            scholarship.ScholarshipApplications = new List<ScholarshipApplication>();
             _scholarshipService.Create(scholarship);
         }
 
         public void Update(Scholarship scholarship)
         {
+            Console.WriteLine("Scholarship contract update");
             _scholarshipService.Update(scholarship);
         }
 
         public void Delete(int id)
         {
+            Console.WriteLine("Scholarship contract delete, ID: " + id);
             _scholarshipService.Delete(id);
         }
 
         public Scholarship GetById(int id)
         {
+            Console.WriteLine("Scholarship contract get by id, ID: " + id);
             return _scholarshipService.GetById(id);
         }
 
         public List<Scholarship> GetAll()
         {
+            Console.WriteLine("Scholarship contract get all");
             return _scholarshipService.GetAll();
         }
     }
