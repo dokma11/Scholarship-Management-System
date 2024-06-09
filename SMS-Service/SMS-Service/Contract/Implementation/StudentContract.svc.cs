5e2c538418761d3896aef7d5eeb36535f60d86a0
dokma11 <vule.dok@gmail.com>
2024-06-09 11:53:49 +0200

    feat: Add logs

--- SMS-Service/SMS-Service/Contract/Implementation/StudentContract.svc.cs
+++ SMS-Service/SMS-Service/Contract/Implementation/StudentContract.svc.cs
@@ -17,27 +17,32 @@ namespace SMS_Service.Contract
 
         public async Task Create(Student student)
         {
+            Console.WriteLine("Student contract create");
             student.ScholarshipApplications = new List<ScholarshipApplication>();
             _studentService.Create(student);
         }
 
         public void Update(Student student)
         {
+            Console.WriteLine("Student contract update");
             _studentService.Update(student);
         }
 
         public void Delete(int id)
         {
+            Console.WriteLine("Student contract delete, id: " +  id);
             _studentService.Delete(id);
         }
 
         public Student GetById(int id)
         {
+            Console.WriteLine("Student contract get by id, id: " + id);
             return _studentService.GetById(id);
         }
 
         public List<Student> GetAll()
         {
+            Console.WriteLine("Student contract get all");
             return _studentService.GetAll();
         }
     }
